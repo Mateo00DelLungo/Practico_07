93607b4c773eae412ec229292a0755143534f5a1
Mateo Del Lungo <mateodellungo@gmail.com>
2024-10-29 18:08:19 -0300

    Adición de un spinner al consultar turnos

--- Proyecto/JS/index.js
+++ Proyecto/JS/index.js
@@ -1,4 +1,10 @@
+const $caption = document.getElementById("descipcion_tabla");
+const $tbody = document.getElementById("turnos_table");
+// loader tabla
+const $loaderdiv = document.getElementById("spinner");
+
 window.addEventListener("load", () => {
+  // loader pagina
   const loader = document.querySelector(".loader");
 
   //le agrega la clase hidden al loader y se esconde
@@ -9,20 +15,36 @@ window.addEventListener("load", () => {
     document.body.removeChild(loader);
   });
 });
+function showSpinner() {
+  $loaderdiv.classList.add("show");
+  $tbody.innerHTML = " ";
+  $caption.innerHTML = "Cargando...";
+}
+function hideSpinner() {
+  $loaderdiv.classList.remove("show");
+  $caption.innerHTML = "Turnos Disponibles:";
+}
 
 function process() {
-  const $tbody = document.getElementById("turnos_table");
-
-  $tbody.innerHTML = "";
-
-  let limit = Math.floor(Math.random() * 8);
-  for (let index = 0; index < limit; index++) {
-    const $tr = document.createElement("tr");
-    $tr.innerHTML = `<th scope='row'>${index + 1} </th>  
-                  <td>Cliente ${index + 1}</td>
-                  <td>9/11</td>
-                  <td>18:00</td>
-    `;
-    $tbody.appendChild($tr);
-  }
+  // cargamos el spinner antes de hacer fetch
+  showSpinner();
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      // lo ocultamos despues de que ya cargaron los datos
+      $tbody.innerHTML = " ";
+      hideSpinner();
+      data.forEach((element) => {
+        const $tr = document.createElement("tr");
+        $tr.innerHTML = `
+        <th scope='row'>${element.id} </th>
+        <td>${element.name}</td>
+        <td>9/11</td>
+        <td>19:00</td>
+        `;
+        $tbody.appendChild($tr);
+      });
+    });
 }
